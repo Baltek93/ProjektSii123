0027b367b7bea8ba3cddf21d695d261dadec7d55
Bartosz <baltek93@gmail.com>
2015-11-21 08:39:22 +0100

    NaprawioneWarzywa

--- app/src/main/java/com/example/bartosz/projektsii123/MainActivity.java
+++ app/src/main/java/com/example/bartosz/projektsii123/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.bartosz.projektsii123;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -57,7 +54,7 @@ public class MainActivity extends Activity {
     }
 
     }
-//Krystian commit
+
 
 
 
